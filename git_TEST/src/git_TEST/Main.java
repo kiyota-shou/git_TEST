8258ec4c6ef516a16eb7a2f4553b35aa44a74972
kiyota-shou <167041400+kiyota-shou@users.noreply.github.com>
2024-12-03 11:24:54 +0900

    Add files via upload

---
+++ git_TEST/src/git_TEST/Main.java
@@ -0,0 +1,5 @@
+package git_TEST;
+
+public class Main {
+
+}
---
+++ git_TEST/src/git_TEST/TEST.java
@@ -0,0 +1,10 @@
+package git_TEST;
+
+public class TEST {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
